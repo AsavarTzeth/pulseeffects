02f77aec63a36f8b153b170a2256cc83b4c6ea0c
AsavarTzeth <asavartzeth@gmail.com>
2018-09-05 10:38:11 +0200

    Enable travis tests of flatpak builds

---
+++ util/flatpak-travis.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# This script is only intended to be run with Travis CI,
+# although it wil likely work just fine locally.
+
+basedir="$(pwd)"
+
+cd ./dist/flatpak
+
+# Use up-to-date remote sources (like a read-only mirror)
+git fetch origin travis
+git checkout travis
+git reset --hard origin/travis
+
+# Run flatpak build against latest commit
+flatpak-builder \
+  --verbose \
+  --disable-updates \
+  --ccache \
+  --install-deps-from=flathub \
+  build com.github.wwmm.pulseeffects.yml
+
+# Reset submodule to master branch
+git checkout master
