47e99276087be371d041f261c97920882c3d9b82
AsavarTzeth <asavartzeth@gmail.com>
2018-09-05 09:35:43 +0200

    Use existing $srcdir with makepkg
    When `makepkg` is executed it uses current directory as its working
    directory. Because of this it will store files in ./src, ./pkg and
    ./pulseeffects.
    
    This is not only annoying to cleanup, it has a chance of conflicting
    with existing files and directories. The solution is to move the
    PKGBUILD to a dedicated sub directory.
    
    In addition this commit will use the existing source directory, instead
    of downloading a second copy of:
    
    - https://github.com/wwmm/pulseeffects.git
    
    Just be careful not to use any operations that delete or cleanup $srcdir
    with makepkg, if testing from your work repo.

--- util/travis-build.sh
+++ util/travis-build.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
+git_root="$(pwd)"
+
+cd ./dist/archlinux/git
+
+if ! [[ -d './src' ]]; then
+  mkdir -p ./src
+fi
+
+if ! [[ -L './src/pulseeffects' ]]; then
+  ln -s "${git_root}" ./src/pulseeffects
+fi
+
 makepkg \
   --syncdeps \
   --needed \
   --noconfirm \
   --install \
-  --noprogressbar
+  --noprogressbar \
+  --holdver \
+  --noextract
